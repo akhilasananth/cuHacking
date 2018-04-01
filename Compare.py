ce942355909553da234d4f74967cbe66f18d2a97
aananth <akhilasananth@gmail.com>
2018-04-01 11:08:40 -0400

    running compare script

--- Compare.py
+++ Compare.py
@@ -1,96 +1,103 @@
 def compare():
-    notDone = True
-    line = 1
+   notDone = True
+   line = 1
 
-    fileC = open("coach.txt", "r")
-    fileT = open("trainee.txt", "r")
+   fileC = open("coach.txt", "r")
+   fileT = open("trainee.txt", "r")
 
-    roll = 0
-    pitch = 0
-    yaw = 0
+   roll = 0
+   pitch = 0
+   yaw = 0
 
+   string = ""
 
-    while(notDone):
-        rollC = fileC.readline().strip().replace(" ", "")
-        if rollC == '':
-            notDone = False
-        else:
-            rollC = int(rollC)
-          #  print("rollc", int(rollC))
-    
+   while(notDone):
+       rollC = fileC.readline().strip().replace(" ", "")
+       if rollC == '':
+           notDone = False
+       else:
+           rollCc = int(rollC)
+         #  print("rollc", int(rollC))
+   
 
-        pitchC = fileC.readline().strip().replace(" ", "")
-        if pitchC == '':
-            notDone = False
-          #  print("didnt get here", pitchC)
-        else:
-            pitchC = int(pitchC)
-            #print("pitchC", int(pitchC))
-
-
-        yawC = fileC.readline().strip().replace(" ", "")
-        if yawC == '':
-            notDone = False
-        else:
-            yawC = int(yawC)
-           # print("yawC", int(yawC))
+       pitchC = fileC.readline().strip().replace(" ", "")
+       if pitchC == '':
+           notDone = False
+         #  print("didnt get here", pitchC)
+       else:
+           pitchCc = int(pitchC)
+           #print("pitchC", int(pitchC))
 
 
-        rollT = fileT.readline().strip().replace(" ", "")
-        if rollC == '':
-            notDone = False
-        else:
-            rollT = int(rollT)
-        #print("rollT", int(rollT))
+       yawC = fileC.readline().strip().replace(" ", "")
+       if yawC == '':
+           notDone = False
+       else:
+           yawCc = int(yawC)
+          # print("yawC", int(yawC))
 
 
-        pitchT = fileT.readline().strip().replace(" ", "")
-        if pitchT == '':
-            notDone = False
-        else:
-            pitchT = int(pitchT)
-       # print("pitchT", int(pitchT))
+       rollT = fileT.readline().strip().replace(" ", "")
+       if rollC == '':
+           notDone = False
+       else:
+           rollTc = int(rollT)
+       #print("rollT", int(rollT))
 
 
-        yawT = fileT.readline().strip().replace(" ", "")
-        if yawT == '':
-            notDone = False
-        else:
-            yawT = int(yawT)
-        #rint("yawT", int(yawT))
-        
-        roll = (roll + (rollC - rollT))/2
-        pitch = (pitch + (pitchC - pitchT))/2
-        yaw = (yaw + (yawC - yawT))/2
-    
-
-        line = line + 3
+       pitchT = fileT.readline().strip().replace(" ", "")
+       if pitchT == '':
+           notDone = False
+       else:
+           pitchTc = int(pitchT)
+      # print("pitchT", int(pitchT))
 
 
-    accuracy = (18 - (roll + pitch + yaw)/3)/18
-    print("You are ",accuracy, "% inaccurate from the coach")
+       yawT = fileT.readline().strip().replace(" ", "")
+       if yawT == '':
+           notDone = False
+       else:
+           yawTc = int(yawT)
+       #rint("yawT", int(yawT))
+       
+       roll = (roll + (rollCc - rollTc))/2
+       pitch = (pitch + (pitchCc - pitchTc))/2
+       yaw = (yaw + (yawCc - yawTc))/2
+   
 
-    if roll == 0:
-        print("You are perfect on your twist")
-    else:
-        if roll > 0:
-            print("you are rolling your wrist too much to the right")
-        else:
-            print("you are rolling your wrist too much to the left")
+       line = line + 3
 
-    if pitch == 0:
-        print("You are perfect on your swing")
-    else:
-        if pitch > 0:
-            print("you are swinging too little")
-        else:
-            print("you are swinging too much")
-    
-    if yaw == 0:
-        print("You are on point")
-    else:
-        if yaw > 0:
-            print("you are too far right")
-        else:
-            print("you are too far left")  
-        
+
+   accuracy = (18 - (roll + pitch + yaw)/3)/18
+   string  += "You are " + str(accuracy) + "% inaccurate from the coach. "
+
+   if roll == 0:
+       string += "You are perfect on your twist. "
+   else:
+       if roll > 0:
+           string += "you are rolling your wrist too much to the right. "
+       else:
+           string += "you are rolling your wrist too much to the left. "
+
+   if pitch == 0:
+       string += "You are perfect on your swing. "
+   else:
+       if pitch > 0:
+          string += "you are swinging too little. "
+       else:
+          string += "you are swinging too much. "
+   
+   if yaw == 0:
+       string += "You are on point."
+   else:
+       if yaw > 0:
+          string += "you are too far right."
+       else:
+          string += "you are too far left."
+
+
+   return string
+
+def main():
+  print(compare())
+main()
